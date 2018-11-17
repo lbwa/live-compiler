0c9d8d273318771e637915c4765f9906495f79d2
Bowen <mr_lbw@outlook.com>
2018-11-17 18:50:40 +0800

    feat: complete initial version

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import * as Babel from '@babel/standalone'
+import './style/App.sass'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      input: '<div className="code"/>',
+      output: '',
+      err: ''
+    }
+  }
+
+  update = e => {
+    try {
+      this.setState({
+        output: this.compiler(e.target.value),
+        err: ''
+      })
+    } catch (e) {
+      this.setState({ err: e.message })
+    }
+  }
+
+  compiler = code => Babel
+    .transform(code, {
+      presets: [
+        'es2015',
+        'react'
+      ]
+    })
+    .code
+
+  componentDidMount () {
+    this.setState(state => ({
+      output: this.compiler(state.input)
+    }))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header>{this.state.err}</header>
+        <main className="container">
+          <textarea
+            onChange={this.update}
+            defaultValue={this.state.input}
+          />
+          <pre>
+            {this.state.output}
+          </pre>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
